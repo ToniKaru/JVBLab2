3f71a43fc9c3af6be14f56c40204c7433b6368fc
ToniKaru <toni.karu@gmail.com>
2022-02-03 22:41:46 +0100

    Add test for when payEmployee fails, then employees are marked as unpaid

--- src/test/java/com/example/EmployeeManagerTest.java
+++ src/test/java/com/example/EmployeeManagerTest.java
@@ -21,8 +21,25 @@ class EmployeeManagerTest {
         var result = employeeManager.payEmployees();
 
         verify(bankServiceMock, times(2)).pay(anyString(), anyDouble());
-        assertEquals(2, result);
         assertTrue(employee1.isPaid());
         assertTrue(employee2.isPaid());
+    }
+
+    @Test
+    void payEmployeeShouldFailWhenBankServiceCannotCompleteTransaction(){
+        BankService bankServiceStub = new BankServiceStub();
+        EmployeeRepository employeeRepoMock = mock(EmployeeRepository.class);
+        when(employeeRepoMock.findAll())
+            .thenReturn(List.of(employee1,employee2));
+        employee1.setPaid(true);
+        employee2.setPaid(true);
+        EmployeeManager manager = new EmployeeManager(employeeRepoMock,bankServiceStub);
+
+        var result = manager.payEmployees();
+
+        verify(employeeRepoMock, times(1)).findAll();
+        assertEquals(0, result);
+        assertFalse(employee1.isPaid());
+        assertFalse(employee2.isPaid());
     }
 }
